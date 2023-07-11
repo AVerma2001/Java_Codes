c4650382caead7b31f0a5bd7d74a2b4ae1ff59a5
AKASH VERMA <130084980+AVerma2001@users.noreply.github.com>
2023-07-11 21:28:52 +0530

    Add files via upload

---
+++ ClassWork.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class ClassWork{
+
+    public static int Knapsack(int wt[], int val[], int W, int n){
+        if(n == 0 || W == 0){
+            return 0;
+        }
+        if(wt[n-1]<=W){ // valid case
+            // include case
+            int ans1 = val[n-1] + Knapsack(wt,val,W-wt[n-1],n-1);
+
+            // exclude case
+            int ans2 = Knapsack(wt,val,W,n-1);
+
+            return Math.max(ans1,ans2);
+
+        }
+        else{ // invalid case W < wt[n-1]
+            return Knapsack(wt,val,W,n-1);
+
+        }
+    }
+
+    public static void main(String args[]){
+        int wt[] = {2,5,1,3,4};
+        int val[] = {15,14,10,45,30};
+        int W = 7;
+        int n = val.length;
+        System.out.println(Knapsack(wt,val,W,n));
+    }
+
+}
